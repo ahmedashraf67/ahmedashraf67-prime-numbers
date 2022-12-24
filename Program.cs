59ce77d754af045fb66bcdb6ae5653672b376fee
ahmedashraf67 <121313403+ahmedashraf67@users.noreply.github.com>
2022-12-24 15:09:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_prime_numbers_from_n1_and_n2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           bool notprime = false;
+            int j,num1 ,num2,i ;
+            Console.WriteLine("Enter your first number");
+             num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("*______*");
+            for (i = num1; i <=num2; i++)
+            {
+                for (j = num1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    { notprime = true; break; }
+                }
+                if (!notprime)
+                    Console.WriteLine("{0}",j);
+                else
+                    notprime = false;
+            }
+               
+        }
+    }
+}
